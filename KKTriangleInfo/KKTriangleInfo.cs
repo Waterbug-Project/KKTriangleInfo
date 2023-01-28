0ab417859c8cc1bf24cd76bc86fa3b1c2ca5e359
Waterbug <waterbugprojectmail@gmail.com>
2023-01-28 17:32:22 -0500

    Prints Hello World message on H start.

---
+++ KKTriangleInfo/KKTriangleInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using KKAPI;
+using KKAPI.MainGame;
+
+namespace KKTriangleInfo
+{
+	[BepInPlugin(GUID, PluginName, Version)]
+	// Tell BepInEx that this plugin needs KKAPI of at least the specified version.
+	// If not found, this plugi will not be loaded and a warning will be shown.
+	[BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+	public class ExamplePlugin : BaseUnityPlugin
+	{
+		public const string PluginName = "KKTriangleInfo";
+		public const string GUID = "org.bepinex.plugins.kktriangleinfo";
+		public const string Version = "0.0.0.0";
+
+		internal static new ManualLogSource Logger;
+
+		private ConfigEntry<bool> _exampleConfigEntry;
+
+		private void Awake()
+		{
+			Logger = base.Logger;
+
+			_exampleConfigEntry = Config.Bind("General", "Enable this plugin", true, "If false, this plugin will do nothing");
+
+			if (_exampleConfigEntry.Value)
+				GameAPI.RegisterExtraBehaviour<KKTriangleInfoController>(GUID);
+		}
+	}
+}
---
+++ KKTriangleInfo/KKTriangleInfoController.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using KKAPI;
+using KKAPI.MainGame;
+using UnityEngine;
+
+namespace KKTriangleInfo
+{
+	class KKTriangleInfoController : GameCustomFunctionController
+	{
+		protected override void OnStartH(HSceneProc inProc, bool inIsFreeH)
+		{
+			UnityEngine.Debug.Log("[KKTriangleInfo] Hello World!");
+		}
+	}
+}
